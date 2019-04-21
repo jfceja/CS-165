87cc4c5ca6226b6300e92ba05994297eee7d2461
Johnny Ceja <jfceja@uci.edu>
2019-04-21 14:21:25 -0700

    Add ?correct? annealing_sort
    Off by one errors in the bounds of the rng interval

--- annealing_sort.cpp
+++ annealing_sort.cpp
@@ -1,16 +1,18 @@
 #include <utility>
 #include <random>
 #include <algorithm> // std::min, std::max
+#include <ctime>
 #include "project1.h"
 
 void annealing_sort(std::vector<int>& nums, const std::vector<int>& temps, const std::vector<int>& reps) {
-	std::mt19937 g1(19937); // arbitrary seed
+	std::mt19937 g1(time(0)); // arbitrary seed
 	
     for (unsigned int j = 0; j < temps.size(); ++j) {
 		for (unsigned int i = 0; i < nums.size() - 1; ++i) {
 			for (unsigned int k = 0; k < reps[j]; ++k) {
 				// let s be a random int in range {i+1, min{n, i + Tj]}
-				std::uniform_int_distribution<int> distribution(i + 1, std::min(nums.size(), i + temps[j]));
+				int r = std::min(nums.size()-1, i + temps[j]);
+				std::uniform_int_distribution<int> distribution(i, std::min(nums.size()- 1, i + temps[j]));
 				unsigned int s = distribution(g1);
 				if (nums[i] > nums[s]) {
 					std::swap(nums[i], nums[s]);
@@ -20,7 +22,8 @@ void annealing_sort(std::vector<int>& nums, const std::vector<int>& temps, const
 		for (unsigned int i = nums.size()-1; i >= 1; --i) {
 			for (unsigned int k = 0; k < reps[j]; ++k) {
 				// let s be a random int in range {max(1, i-Tj), i-1}
-				std::uniform_int_distribution<int> distribution(std::max(0, i - temps[j]), i - 1);
+				int r = std::max(0, ((int)i) - temps[j])-1;
+				std::uniform_int_distribution<int> distribution(std::max(0, ((int)i) - temps[j]-1), i - 1);
 				unsigned int s = distribution(g1);
 				if (nums[s] > nums[i]) {
 					std::swap(nums[i], nums[s]);
